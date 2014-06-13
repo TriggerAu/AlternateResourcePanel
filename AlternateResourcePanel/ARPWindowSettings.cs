78261ace30745b2050fef5a493be304fbef154c9
TriggerAu <dtregoning@outlook.com.au>
2014-06-13 23:21:36 +1000

    Changed version check to use a BackgroundWorker delegate and completed event args (Fixes #20)

--- AlternateResourcePanel/ARPWindowSettings.cs
+++ AlternateResourcePanel/ARPWindowSettings.cs
@@ -498,10 +498,18 @@ namespace KSPAlternateResourcePanel
             GUILayout.Label(settings.VersionCheckDate_AttemptString, Styles.styleTextGreen);
             GUILayout.Label(settings.Version, Styles.styleTextGreen);
 
-            if (settings.VersionAvailable)
-                GUILayout.Label(String.Format("{0} @ {1}", settings.VersionWeb, settings.VersionCheckDate_SuccessString), Styles.styleTextYellowBold);
+            if (settings.VersionCheckRunning)
+            {
+                Int32 intDots = Convert.ToInt32(Math.Truncate(DateTime.Now.Millisecond / 250d)) + 1;
+                GUILayout.Label(String.Format("{0} Checking", new String('.', intDots)), KACResources.styleVersionHighlight);
+            }
             else
-                GUILayout.Label(String.Format("{0} @ {1}", settings.VersionWeb, settings.VersionCheckDate_SuccessString), Styles.styleTextGreen);
+            {
+                if (settings.VersionAvailable)
+                    GUILayout.Label(String.Format("{0} @ {1}", settings.VersionWeb, settings.VersionCheckDate_SuccessString), KACResources.styleVersionHighlight);
+                else
+                    GUILayout.Label(String.Format("{0} @ {1}", settings.VersionWeb, settings.VersionCheckDate_SuccessString), KACResources.styleContent);
+            }
             GUILayout.EndVertical();
             GUILayout.EndHorizontal();
 
