60e7263e92de34b4d66eacfcbedd64505f3c3b38
TriggerAu <dtregoning@outlook.com.au>
2014-10-08 22:49:40 +1100

    Fixed about window size when new version - take 2

--- AlternateResourcePanel/ARPWindowSettings.cs
+++ AlternateResourcePanel/ARPWindowSettings.cs
@@ -203,7 +203,7 @@ namespace KSPAlternateResourcePanel
                     DrawWindow_Staging();
                     break;
                 case SettingsTabs.About:
-                    WindowHeight = 246 + (settings.VersionAvailable?mbARP.windowDebug.intTest1:0);
+                    WindowHeight = 246 + (settings.VersionAvailable?24:0);
                     DrawWindow_About();
                     break;
             }
